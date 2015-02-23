64c009b2e033e249d3af32827b0fe65189415d1f
Britt Jakobsen <bjakob09@student.aau.dk>
2015-02-23 17:52:12 +0100

    Made the glossaries work on Windows

--- report/nomenclatur_run.bat
+++ report/nomenclatur_run.bat
@@ -1,5 +1,2 @@
-pdflatex DaVinci_main.tex
-makeindex DaVinci_main.nlo -s formalia/nomencl.ist -o DaVinci_main.nls
-pdflatex DaVinci_main.tex
-
-pause
+makeindex DaVinci_main.nlo -s nomencl.ist -o DaVinci_main.nls
+makeindex -s DaVinci_main.ist -t DaVinci_main.slg -o DaVinci_main.syi DaVinci_main.syg
